8e31bb3db9824bd011a1f38805b0fda39e72804f
wjH-3 <jcnx25@durham.ac.uk>
2024-10-27 02:19:16 +0000

    mpv script to auto open after getting link

---
+++ mpv_auto.py
@@ -0,0 +1,21 @@
+import mpv
+
+def play_in_mpv(link):
+    """Simple function to play a video link in MPV"""
+    player = mpv.MPV(
+        input_default_bindings=True,
+        input_vo_keyboard=True,
+        osc=True,
+        config=True,
+        geometry="1280x720"
+    )
+
+    player['fullscreen'] = False  # Start windowed, can be toggled with 'f' key
+    player['keep-open'] = True    # Don't close player when video ends
+
+    try:
+        player.play(link)
+        time.sleep(0.5)
+        player.wait_for_playback()
+    finally:
+        player.terminate()
