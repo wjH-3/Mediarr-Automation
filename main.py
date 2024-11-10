40bc87b1087af97afe88916c2d20035ebc188168
wjH-3 <jcnx25@durham.ac.uk>
2024-11-10 02:22:06 +0000

    no longer need to get user and profile browser config for non_ani; as a result dmm_library option also removed

--- main.py
+++ main.py
@@ -1,48 +1,15 @@
 import os
 import json
 import ani
-import non_ani
+import non_aniV2
 import sys
 import time
 import unrestrict
 import RD
-import DMM_library
 import torrentLibrary
 from mpv_auto import play_in_mpv
 
-CONFIG_PATH = 'config.json'
 TOKEN_PATH = 'token.json'
-
-def get_config():
-    if getattr(sys, 'frozen', False):  # Check if running as an executable
-        config_dir = os.path.dirname(sys.executable)
-    else:
-        config_dir = os.path.dirname(__file__)
-
-    config_path = os.path.join(config_dir, CONFIG_PATH)
-
-    if os.path.exists(config_path):
-        with open(config_path, 'r') as f:
-            return json.load(f)
-    else:
-        return create_config(config_path)
-
-def create_config(config_path):
-    print("First-time setup. Please input the necessary information. The config will be stored locally in 'config.json'. (Note: Input is case-sensitive.)")
-    print("You can find the relevant info at: chrome://version/, under 'Profile Path'.")
-    user = input("Enter your system user name: ").strip()
-    profile = input("Enter your Chrome profile directory name: ").strip()
-    config = {'user': user, 'profile': profile}
-
-    try:
-        with open(config_path, 'w') as f:
-            json.dump(config, f)
-        print("Configuration saved successfully.\n")
-    except IOError as e:
-        print(f"Unable to write config file. Error: {e}")
-        print("You'll need to enter this information each time you run the program.")
-
-    return config
 
 def get_token():
     if getattr(sys, 'frozen', False):
@@ -74,46 +41,17 @@ def create_token(token_path):
 
     return token
 
-def run_non_ani(user, profile):
-    # Preserve the original sys.argv
-    original_argv = sys.argv.copy()
-    
-    # Set sys.argv for non_ani.py
-    sys.argv = [sys.argv[0], user, profile]
-    
-    try:
-        # Run non_ani.main()
-        non_ani.main()
-    finally:
-        # Restore the original sys.argv
-        sys.argv = original_argv
-
-def open_DMM_library(user, profile):
-    # Preserve the original sys.argv
-    original_argv = sys.argv.copy()
-    
-    # Set sys.argv for non_ani.py
-    sys.argv = [sys.argv[0], user, profile]
-    
-    try:
-        # Run non_ani.main()
-        DMM_library.main()
-    finally:
-        # Restore the original sys.argv
-        sys.argv = original_argv
-
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
     try:
-        config = get_config()
         token = get_token()
 
         while True:
             clear_screen()
             time.sleep(0.5)
-            options = print("Options:\n1. Search Movies/TV Shows\n2. Get Movies/TV Shows in Library\n3. Add Magnet Link\n4. Unrestrict Link\n5. Go DMM Library")
+            options = print("Options:\n1. Search Movies/TV Shows\n2. Get Movies/TV Shows in Library\n3. Add Magnet Link\n4. Unrestrict Link")
             choice = input("Enter Option Number: ")
             if choice == '1':
                 while True:
@@ -123,7 +61,7 @@ def main():
                         ani.main()
                         break
                     elif media_type == 'N':
-                        run_non_ani(config['user'], config['profile'])
+                        non_aniV2.main()
                         break
                     else:
                         print("Invalid choice. Please enter A for Anime or N for Non-Anime.")
@@ -137,11 +75,8 @@ def main():
             if choice == '4':
                 unrestrict.main()
                 continue
-            if choice == '5':
-                open_DMM_library(config['user'], config['profile'])
-                continue
             else:
-                print("\nInvalid input. Please enter a Number from 1 to 5.")
+                print("\nInvalid input. Please enter a Number from 1 to 4.")
                 time.sleep(2)
     
     except Exception as e:
