feac6d5bcab27536aa1afdaff25bb62f0007e4a4
wjH-3 <jcnx25@durham.ac.uk>
2024-12-03 20:32:52 +0000

    check and catch error during select_files; accept magnet links instead of hashes; formatting

--- components/instant.py
+++ components/instant.py
@@ -3,13 +3,12 @@ import time
 
 VIDEO_EXTENSIONS = ('.avi', '.mkv', '.mp4', '.mov', '.wmv', '.flv', '.webm', '.m4v', '.mpeg', '.mpg')
 
-def add_magnet(api_token, magnet_hash):
-        url = "https://api.real-debrid.com/rest/1.0/torrents/addMagnet"
-        headers = {"Authorization": f"Bearer {api_token}"}
-        data = {"magnet": f"magnet:?xt=urn:btih:{magnet_hash}"}
-        response = requests.post(url, headers=headers, data=data)
-        #print(response.json())
-        return response.json()
+def add_magnet(api_token, magnet_link):
+    url = "https://api.real-debrid.com/rest/1.0/torrents/addMagnet"
+    headers = {"Authorization": f"Bearer {api_token}"}
+    data = {"magnet": f"{magnet_link}"}
+    response = requests.post(url, headers=headers, data=data)
+    return response.json()
 
 def get_torrent_info(api_token, torrent_id):
     url = f"https://api.real-debrid.com/rest/1.0/torrents/info/{torrent_id}"
@@ -24,7 +23,9 @@ def select_files(api_token, torrent_id, file_ids):
     response = requests.post(url, headers=headers, data=data)
     if response.status_code == 204:
         return {"success": True}
-    print(response.json())
+    json = response.json()
+    if 'error' in json and json['error']:
+        print(f"Error: {json['error']}")
     return response.json()
 
 def is_video(filename):
@@ -37,8 +38,8 @@ def delete_torrent(api_token, torrent_id):
     if response.status_code == 204:
         return
     
-def pseudo_instant_check(magnet_hash, api_token):    
-    add_result = add_magnet(api_token, magnet_hash)
+def pseudo_instant_check(magnet_link, api_token):    
+    add_result = add_magnet(api_token, magnet_link)
     # Check if 'error' exists in the response and handle it
     if 'error' in add_result and add_result['error']:
         print(f"Error: {add_result['error']}")
@@ -62,20 +63,20 @@ def pseudo_instant_check(magnet_hash, api_token):
         return False, torrent_id
     
 def main():
-    api_token = input("Enter API token: ")
+    api_token = input("Enter RD API Token: ")
     
     # TEST HASHES:
-    # Arcane S02E04 1080p WEB-DL FLUX (CACHED): 8cf843d57f7702e6176a419378135f5130706db7
-    # Tenet 1080p BluRay REMUX FraMeSToR (UNCACHED): 571d85882b7733c2d16c0dd534721f5820ba9592
-    # The Outsider S01 1080p BluRay REMUX PmP (CACHED): ef2e20adbf8a6af9b72708b75c1b01e5cc7794b5
-    # Black Mirror S03 1080i BluRau REMUX EPSiLON (UNCACHED): a1217dc6b392cb7a1d47f901e92d3bd7e5c11c22
-    # INFRINGING FILE (Error Code 35): 0c234063653cb6ae7437d70a388751da1821ef58
-    # INTERNAL ERROR (Error Code -1): aa0917dfdf60a3fb6f4fb7cad679783ffc194561
-    # INTERNAL ERROR (Error Code -1) (NOT Invalid Magnet): 13651b8db7074da3aa0d1bfebb479f75fef03ca2
-    magnet_hash = input("Enter Magnet Hash: ")
+    # Arcane S02E04 1080p WEB-DL FLUX (CACHED): magnet:?xt=urn:btih:8cf843d57f7702e6176a419378135f5130706db7
+    # Tenet 1080p BluRay REMUX FraMeSToR (UNCACHED): magnet:?xt=urn:btih:571d85882b7733c2d16c0dd534721f5820ba9592
+    # The Outsider S01 1080p BluRay REMUX PmP (CACHED): magnet:?xt=urn:btih:ef2e20adbf8a6af9b72708b75c1b01e5cc7794b5
+    # Black Mirror S03 1080i BluRau REMUX EPSiLON (UNCACHED): magnet:?xt=urn:btih:a1217dc6b392cb7a1d47f901e92d3bd7e5c11c22
+    # INFRINGING FILE (Error Code 35): magnet:?xt=urn:btih:0c234063653cb6ae7437d70a388751da1821ef58
+    # INTERNAL ERROR (Error Code -1): magnet:?xt=urn:btih:aa0917dfdf60a3fb6f4fb7cad679783ffc194561
+    # INTERNAL ERROR (Error Code -1) (NOT Invalid Magnet): magnet:?xt=urn:btih:13651b8db7074da3aa0d1bfebb479f75fef03ca2
+    magnet_link = input("Enter Magnet Link: ")
 
     start_time = time.perf_counter()
-    result, torrent_id = pseudo_instant_check(magnet_hash, api_token)
+    result, torrent_id = pseudo_instant_check(magnet_link, api_token)
     delete_torrent(api_token, torrent_id)
     end_time = time.perf_counter()
     if result is True:
